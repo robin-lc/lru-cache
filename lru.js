4576de95238f56e5d75811b9e69de8567de4a464
robin-lc <rb.lecorre@gmail.com>
2019-02-10 13:19:16 +0100

    implémentation get() + del() et tests

--- lru.js
+++ lru.js
@@ -5,6 +5,7 @@ class LRU {
 	constructor() {
 		this.head = null;
 		this.tail = null;
+		this.cache = {};
 	}
 
 	set(key, value) {
@@ -19,12 +20,42 @@ class LRU {
 			this.head.next = node;
 			this.head = node;
 		}
+		this.cache[key] = this.head;
 	}
 
 	get(key) {
+		
+		if(this.cache[key] !== null) {
+
+			const value = this.cache[key].value;
+
+			//suite à l'accès, on remonte le noeud en haut de la chaine en le supprimant puis re-créant
+			this.del(key);
+			this.set(key, value);
+
+			return value;
+		}
+		return false;
 	}
 
 	has(key) {
+	}
+
+	del(key) {
+
+		let nodeToDelete = this.cache[key];
+
+		if(nodeToDelete.next !== null) //si le noeud à supprimer n'est pas en haut de la chaine
+			nodeToDelete.next.prev = nodeToDelete.prev; //on modifie le prev du suivant par le prev du noeud à supprimer
+		else
+			this.head = nodeToDelete.prev; //si le noeud est en haut de la chaine, on modifie juste notre head par le prev du noeud à supprimer
+
+		if(nodeToDelete.prev !== null) //si le noeud à supprimer n'est pas en bas de la chaine
+			nodeToDelete.prev.next = nodeToDelete.next; //on modifie le next du précédent par le next du noeud à supprimer
+		else
+			this.tail = nodeToDelete.next; //si le noeud est en bas de la chaine, on modifie juste notre tail par le next du noeud à supprimer
+		
+		delete(this.cache[key]);
 	}
 }
 
--- tests/lru.test.js
+++ tests/lru.test.js
@@ -16,3 +16,52 @@ test('set()', () => {
 	expect(lru.head.value).toBe('c');
 	expect(lru.tail.value).toBe('a');
 });
+
+
+test('get()', () => {
+
+	const lru = new LRU();
+
+	lru.set("A", "a");
+	lru.set("B", "b");
+	lru.set("C", "c"); 
+	expect(lru.head.value).toBe('c');
+	expect(lru.tail.value).toBe('a');
+	//current cache: "C, B, A"
+
+	lru.get('B');
+	expect(lru.head.value).toBe('b');
+	//current cache: "B, C, A"
+
+	lru.get('A');
+	expect(lru.head.value).toBe('a');
+	expect(lru.tail.value).toBe('c');
+	//current cache: "A, B, C"
+
+	lru.get('A');
+	expect(lru.head.value).toBe('a');
+	expect(lru.tail.value).toBe('c');
+	//current cache: "A, B, C"
+});
+
+test('del()', () => {
+
+	const lru = new LRU();
+
+	lru.set("A", "a");
+	lru.set("B", "b");
+	lru.set("C", "c"); 
+	expect(lru.head.value).toBe('c');
+	expect(lru.tail.value).toBe('a');
+	//current cache: "C, B, A"
+
+	lru.del('C');
+	expect(lru.head.value).toBe('b');
+	expect(lru.tail.value).toBe('a');
+	//current cache: "B, A"
+
+	lru.del('A');
+	expect(lru.head.value).toBe('b');
+	expect(lru.tail.value).toBe('b');
+	//current cache: "B"
+});
